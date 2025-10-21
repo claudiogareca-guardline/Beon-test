2c29989b65528cbe623b809a50ed552de13f82b2
Claudio Gareca <claudio.gareca@sddevs.io>
2025-10-21 16:53:28 -0400

    add files

---
+++ src/SecureFlight.Core/Error.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecureFlight.Core;
+
+public class Error
+{
+    [Required]
+    public ErrorCode Code { get; set; }
+
+    [Required]
+    public string Message { get; set; }
+}
